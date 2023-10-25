97e7676fe808a86f4ace725def9ee2fd01c5792e
hhhhhhjs <1512403978@qq.com>
2023-10-25 16:43:03 +0800

    vue-router

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import {createRouter,createWebHashHistory} from 'vue-router'
+import {createRouter,createWebHashHistory,createWebHistory} from 'vue-router'
 import HomeComp from '@/components/Home.vue'
 import LoginComp from '@/components/Login.vue'
 
@@ -11,7 +11,7 @@ const routes = [
 
 const router = createRouter({
 
-  history: createWebHashHistory(),
+  history: createWebHistory(),
   routes:routes 
 })
 export default router
